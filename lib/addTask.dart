dce8554e13ccc24af6fbc4c9972ad47315554498
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-15 18:55:31 +0530

    add datetimeformfield(error)

--- lib/addTask.dart
+++ lib/addTask.dart
@@ -2,59 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:flutter_datetime_formfield/flutter_datetime_formfield.dart';
 
 class AddTaskPage extends StatelessWidget {
+  final dateFormat = DateFormat("yyyy-MM-dd");
+  final timeFormat = DateFormat("HH:mm");
 
-final dateFormat = DateFormat("yyyy-MM-dd");
-final timeFormat = DateFormat("HH:mm");
+  final DateTime initialDateTime;
+  DateTime _dateTime;
 
-Widget _taskTitleFormField(){
-  return  TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'title is required';
-              }
-            },
-             decoration: InputDecoration(labelText: 'Product title')
-          );
-}
+  AddTaskPage(this.initialDateTime);
 
-Widget _taskDescriptionFormField(){
-  return  TextFormField(
-             decoration: InputDecoration(labelText: 'Product Decription')
-          );
-}
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-
-Widget _taskDatePicker(){
-   return 
-      DateTimeField(
-        decoration: InputDecoration(labelText: 'Date'),
-        format: dateFormat,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
+  Widget _taskTitleFormField() {
+    return TextFormField(
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'title is required';
+          }
         },
-      );
+        decoration: InputDecoration(labelText: 'Product title'));
+  }
+
+  Widget _taskDescriptionFormField() {
+    return TextFormField(
+        decoration: InputDecoration(labelText: 'Product Decription'));
+  }
+
+  Widget _taskDatePicker() {
+    // return DateTimeField(
     
-}
+    //   decoration: InputDecoration(labelText: 'Date'),
+    //   format: dateFormat,
+    //   onShowPicker: (context, currentValue) {
+    //     return showDatePicker(
+    //         context: context,
+    //         firstDate: DateTime(1900),
+    //         initialDate: currentValue ?? DateTime.now(),
+    //         lastDate: DateTime(2100));
+    //   },
+    // );
+    return DateTimeFormField(
+                initialValue: initialDateTime,
+                label: "Date Time",
+                validator: (DateTime dateTime) {
+                  if (dateTime == null) {
+                    return "Date Time Required";
+                  }
+                  return null;
+                },
+                onSaved: (DateTime dateTime) => _dateTime = dateTime,
+              );
+  }
 
-Widget _taskTimePicker(){
-  return   DateTimeField(
-        decoration: InputDecoration(labelText: 'Time'),
-        format: timeFormat,
-        onShowPicker: (context, currentValue) async {
-          final time = await showTimePicker(
-            context: context,
-            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
-          );
-          return DateTimeField.convert(time);
-        },
-      );
-}
+  Widget _taskTimePicker() {
+    return DateTimeField(
+      decoration: InputDecoration(labelText: 'Time'),
+      format: timeFormat,
+      onShowPicker: (context, currentValue) async {
+        final time = await showTimePicker(
+          context: context,
+          initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+        );
+        return DateTimeField.convert(time);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,23 +79,23 @@ Widget _taskTimePicker(){
       ),
       body: Container(
         margin: EdgeInsets.all(30.0),
-        child: Form(
-          child: Column(children: <Widget>[
-            _taskTitleFormField(),
-            _taskDescriptionFormField(),
-            _taskDatePicker(),
-            _taskTimePicker(),
-            SizedBox(
-              height: 10.0
-            ),
-            RaisedButton(
-                    child: Text('Save'),
-                    color: Theme.of(context).accentColor,
-                    textColor: Colors.white,
-                    onPressed: (){},
-                    )
-          ]),
-        ),
+        child: ListView(children: <Widget>[
+          Form(
+            child: Column(children: <Widget>[
+              _taskTitleFormField(),
+              _taskDescriptionFormField(),
+              _taskDatePicker(),
+              _taskTimePicker(),
+              SizedBox(height: 10.0),
+              RaisedButton(
+                child: Text('Save'),
+                color: Theme.of(context).accentColor,
+                textColor: Colors.white,
+                onPressed: () {},
+              )
+            ]),
+          ),
+        ]),
       ),
     );
   }
